4863eadbbf520e28356f6c3df69ff1d49253bc19
root <root@11LAP5CD31318N3.indra.es>
2023-11-28 10:59:00 +0000

    new commit

---
+++ bloblang/bloblang.go
@@ -0,0 +1,20 @@
+package bloblang
+
+import "github.com/benthosdev/benthos/v4/public/bloblang"
+
+func init() {
+	lenSpec := bloblang.NewPluginSpec().
+		Category("String Manipulation").
+		Description("Return the length of the string.")
+
+	if err := bloblang.RegisterMethodV2("lenString", lenSpec,
+		func(args *bloblang.ParsedParams) (bloblang.Method, error) {
+			return bloblang.StringMethod(func(s string) (any, error) {
+				return len(s), nil
+			}), nil
+		},
+	); err != nil {
+		panic(err)
+	}
+
+}
